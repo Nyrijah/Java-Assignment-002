c178602e37a566c3b1aca0eb20cecba2bf6da074
Nyrijah <Nyrijah@gmail.com>
2023-09-27 11:48:20 -0700

    Sorry it is late

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,18 @@
 import java.util.Scanner;
 
+
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
-
+        while ( true ) {
+            System.out.print("Enter up to 10 digets for a polygon(no words Please:");
+            Scanner s = new Scanner(System.in);
+            int numberSides = Integer.parseInt(s.nextLine());
+            double calcualateAngle = 360.0 / numberSides;
+            double calcualateLength = (calcualateAngle / 2);
+            double side = 2 * Math.sin(Math.toRadians(calcualateLength));
+            double polygon = numberSides * side;
+            double Pi = polygon / 2;
+            System.out.printf("Estimate for PI: %.15f Real PI: %.15f%n", Pi, Math.PI);
+        }
     }
 }
